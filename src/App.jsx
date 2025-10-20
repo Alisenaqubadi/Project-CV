c1736c08e6b0964cc0139254d19f2b823d7c0c0a
Sayed Ali Sena <131773898+Alisenaqubadi@users.noreply.github.com>
2025-10-20 23:25:54 +0500

    In progress the basic is half finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+function Header() {
+  return(
+    <>
+      <div className="header"> <h1>The CV Maker</h1> </div>
+    </>
+  )
+}
 
+function GeneralInfo(){
+  return(
+    <div className="GeneralInfo input-form">
+      <input type="text" id="Name"className="nice-input" placeholder="Name"/>
+      <input type="email" id="Email" className="nice-input" placeholder="Email"/>
+      <input type="tel" id="Phone" className="nice-input" placeholder="Phone"/>
+      <input type="text" id="Linkedin" className="nice-input" placeholder="Linkedin"/>
+    </div>
+  )
+}
+
+function Education(){
+  return(
+    <div className="Education input-form">
+      <input type="text" id="Institution" className="nice-input" placeholder="Institution"/>
+      <div className="date-parent">
+        <input type="date" id="Institution-start" className="nice-input"/>
+      </div>
+    </div>
+  )
+}
+
+
+function Current(props){
+  if(props.show == 1){
+    return(<GeneralInfo />)
+  } else if (props.show == 2) {
+    return(<Education />)
+  }
+}
+
+function Body(){
+  const [show, setShow] = useState(1)
+
+  return(
+    <>
+      <div className="body">
+        <div className="form">
+          <h2>Make you CV</h2>
+          <Current show={show}/>
+          <div className="center-btn">
+            <button className="nice-btn" onClick={() => setShow(prev => prev + 1)}>Next</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Body />
     </>
   );
 }
